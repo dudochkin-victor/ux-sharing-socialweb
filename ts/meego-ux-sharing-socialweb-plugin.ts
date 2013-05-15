9162706baa7fa84cbf4464be42c0f7ae4297b534
Victor Dudochkin <dudochkin.victor@gmail.com>
2013-05-15 14:54:48 +0800

    Initial commit

---
+++ ts/meego-ux-sharing-socialweb-plugin.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>SwSharingService</name>
+    <message>
+        <location filename="../../plugin/swsharingservice.cpp" line="123"/>
+        <source>Web</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ ts/meego-ux-sharing-socialweb-qml.ts
@@ -0,0 +1,437 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CollectionsPicker</name>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="64"/>
+        <source>Choose an album to upload to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="165"/>
+        <source>Add a new album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="187"/>
+        <source>Add a title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="206"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="305"/>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="307"/>
+        <source>Photo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="309"/>
+        <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CollectionsPicker.qml" line="311"/>
+        <source>Upload %1 to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CreateCollection</name>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="61"/>
+        <source>Choose a name for your new album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="106"/>
+        <source>Album name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="135"/>
+        <source>Create album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="145"/>
+        <location filename="../../qml/CreateCollection.qml" line="232"/>
+        <source>Collection creation: General error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="152"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="240"/>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="242"/>
+        <source>Photo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="244"/>
+        <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/CreateCollection.qml" line="246"/>
+        <source>Upload %1 to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MediaUploader</name>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="32"/>
+        <source>No album</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="218"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="249"/>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="251"/>
+        <source>Photo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="253"/>
+        <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="255"/>
+        <source>Upload %1 to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="273"/>
+        <source>File %1 of %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="293"/>
+        <source>&lt;b&gt;Album:&lt;/b&gt; %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="298"/>
+        <source>Upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/MediaUploader.qml" line="300"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>facebook_image_multi</name>
+    <message>
+        <location filename="../../qml/facebook_image_multi.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>facebook_image_single</name>
+    <message>
+        <location filename="../../qml/facebook_image_single.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>facebook_video_multi</name>
+    <message>
+        <location filename="../../qml/facebook_video_multi.qml" line="19"/>
+        <source>Video name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>facebook_video_single</name>
+    <message>
+        <location filename="../../qml/facebook_video_single.qml" line="19"/>
+        <source>Video name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>flickr_image_multi</name>
+    <message>
+        <location filename="../../qml/flickr_image_multi.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_multi.qml" line="23"/>
+        <source>Photo description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_multi.qml" line="26"/>
+        <source>Photo tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_multi.qml" line="55"/>
+        <source>Only you (private)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_multi.qml" line="85"/>
+        <source>Your friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_multi.qml" line="117"/>
+        <source>Your family</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_multi.qml" line="151"/>
+        <source>Anyone (public)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>flickr_image_single</name>
+    <message>
+        <location filename="../../qml/flickr_image_single.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_single.qml" line="23"/>
+        <source>Photo description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_single.qml" line="26"/>
+        <source>Photo tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_single.qml" line="55"/>
+        <source>Only you (private)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_single.qml" line="85"/>
+        <source>Your friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_single.qml" line="117"/>
+        <source>Your family</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_image_single.qml" line="151"/>
+        <source>Anyone (public)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>flickr_video_multi</name>
+    <message>
+        <location filename="../../qml/flickr_video_multi.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_multi.qml" line="23"/>
+        <source>Photo description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_multi.qml" line="26"/>
+        <source>Photo tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_multi.qml" line="55"/>
+        <source>Only you (private)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_multi.qml" line="85"/>
+        <source>Your friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_multi.qml" line="117"/>
+        <source>Your family</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_multi.qml" line="151"/>
+        <source>Anyone (public)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>flickr_video_single</name>
+    <message>
+        <location filename="../../qml/flickr_video_single.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_single.qml" line="23"/>
+        <source>Photo description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_single.qml" line="26"/>
+        <source>Photo tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_single.qml" line="55"/>
+        <source>Only you (private)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_single.qml" line="85"/>
+        <source>Your friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_single.qml" line="117"/>
+        <source>Your family</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/flickr_video_single.qml" line="151"/>
+        <source>Anyone (public)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>photobucket_image_multi</name>
+    <message>
+        <location filename="../../qml/photobucket_image_multi.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/photobucket_image_multi.qml" line="23"/>
+        <source>Photo description</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>photobucket_image_single</name>
+    <message>
+        <location filename="../../qml/photobucket_image_single.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/photobucket_image_single.qml" line="23"/>
+        <source>Photo description</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>smugmug_image_multi</name>
+    <message>
+        <location filename="../../qml/smugmug_image_multi.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/smugmug_image_multi.qml" line="23"/>
+        <source>Photo tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>smugmug_image_single</name>
+    <message>
+        <location filename="../../qml/smugmug_image_single.qml" line="20"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/smugmug_image_single.qml" line="23"/>
+        <source>Photo tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>twitter_image_multi</name>
+    <message>
+        <location filename="../../qml/twitter_image_multi.qml" line="19"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>twitter_image_single</name>
+    <message>
+        <location filename="../../qml/twitter_image_single.qml" line="19"/>
+        <source>Photo name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>youtube_video_multi</name>
+    <message>
+        <location filename="../../qml/youtube_video_multi.qml" line="19"/>
+        <source>Video name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/youtube_video_multi.qml" line="22"/>
+        <source>Video description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/youtube_video_multi.qml" line="25"/>
+        <source>Video tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>youtube_video_single</name>
+    <message>
+        <location filename="../../qml/youtube_video_single.qml" line="19"/>
+        <source>Video name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/youtube_video_single.qml" line="22"/>
+        <source>Video description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/youtube_video_single.qml" line="25"/>
+        <source>Video tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
